6ae78303991ddbe89723f5305c8fbaba8f87bbda
Matheus Fortini <matheusfortini7@gmail.com>
2024-03-31 17:18:12 -0300

    Adicionar mecanismos para lidar com erros

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
+
+  private
+
+  # Serve para tratar erro em caso de requisição para o delete não funcionar
+  def not_destroyed(e)
+    render json: {erros: e.record.errors}, status: :unprocessable_entity
+  end
 end
